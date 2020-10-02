185381574d3ebc4cfc408fbb7a639942eaf9bed9
Samuel Rembisz <sjrembisz07@gmail.com>
2020-10-02 19:57:41 -0400

    Removed testing comments

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-from bs4 import BeautifulSoup
 from sys import exit
 from datetime import datetime
 import json
@@ -22,7 +21,6 @@ else:
 
 # Contacting the API to get meals on specified day
 url = f"https://menus.sodexomyway.com/BiteMenu/MenuOnly?menuId=16653&locationId=27747017&whereami=http://masondining.sodexomyway.com/dining-near-me/ikes&startDate={specifiedDate}"
-#print(url)
 
 options = webdriver.ChromeOptions()
 options.add_argument('headless')
@@ -37,12 +35,8 @@ jsonUnparsedTag = driver.find_element_by_xpath("/html/body/div/div[11]")
 if jsonUnparsedTag is not None:
     jsonUnparsed = jsonUnparsedTag.text
 
-    #print(datetime.strptime(specifiedDate, '%m-%d-%Y').strftime('%w'))
-
     #Get the meal data for this specific day from it's day of the week (their api returns the whole week -_-)
     jsonParsed = json.loads(jsonUnparsed)[int(datetime.strptime(specifiedDate, '%m-%d-%Y').strftime('%w'))].get('dayParts')
-
-    #print(jsonParsed)
 
     meals = {}
 
@@ -50,7 +44,7 @@ if jsonUnparsedTag is not None:
     for dayPart in jsonParsed:
         for part in dayPart['courses']:
             if part['courseName'] == "-" or part['courseName'] is None:
-                pass
+                continue
             
             menuItems = part['menuItems']
 
@@ -61,8 +55,8 @@ if jsonUnparsedTag is not None:
                 course = item["course"]
                 formalName = item["formalName"]
 
-                if course is None:
-                    pass
+                if course is None or course == "None" or course == "-" or course == "null":
+                    continue
 
                 #Logic to create custom hashmaps for each station and its items
                 stationAndName = {}
@@ -85,58 +79,5 @@ if jsonUnparsedTag is not None:
             for mealName in mealNames:
                 print(f"        -{mealName}")
         print()
-
-    #print(jsonParsed)
 else:
     print(f"There was an error contacting the API, please try later or try a different date ({specifiedDate})")
-
-
-
-
-
-###Attempted to webscrape, found the API...
-###
-###button = driver.find_element_by_xpath("/html/body/div[2]/div/div[2]/div[2]/div[1]/a[1]")
-###
-###button.click()
-###
-###
-###body = driver.page_source
-###
-###soup = BeautifulSoup(body, "lxml")
-###
-###
-####Getting meal data to be parsed
-###accordion = []
-###
-###meals = {"breakfast", "lunch", "dinner"}
-###
-###for meal in meals:
-###    accordion.append(soup.find('div', attrs={"class" : f"accordion-block {meal}"}))
-###
-####Parsing meal data
-###
-###items = {}
-###
-###for meal in accordion:
-###    listOfStations = meal.findAll('div', attrs={"class": "bite-menu-course"})
-###    listOfItems = meal.findAll('ul', attrs={"class": "bite-menu-item"})
-###
-###    listOfStationNames = []
-###    for station in listOfStations:
-###        listOfStationNames.append(station.find("h5").string)
-###
-###    stationItems = []
-###    for items in listOfItems:
-###        listOfSubItems = items.findAll("li")
-###        listOfItemNames = []
-###        for item in listOfSubItems:
-###            listOfItemNames.append(items.find("div", attrs={"class", "col-xs-9"}).find("a").text)
-###        stationItems.append(listOfItemNames)
-###
-###    print(stationItems)
-###
-###
-###
-###
-###
